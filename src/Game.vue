1a053c48275296e0fea3b1587405c3abc3e4ed9c
Poijalex <99257781+Poijalex@users.noreply.github.com>
2022-02-08 13:47:54 +0100

    Update Game.vue

--- src/Game.vue
+++ src/Game.vue
@@ -113,7 +113,7 @@ function completeRow() {
       setTimeout(() => {
         grid = genResultGrid()
         showMessage(
-          ['Et oui c est ton ANNIVersaire Bon anniversaire'][
+          ['Eh oui c est ton ANNIVersaire ! Bon anniversaire :)'][
             currentRowIndex
           ],
           -1
